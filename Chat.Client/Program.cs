7eb5d66337f6bf01aada2d9061be8860b8dc3896
r.shpak <r.shpak@steel.mil.ua>
2024-09-27 16:46:14 +0300

    Chat base implementation

--- Chat.Client/Program.cs
+++ Chat.Client/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Chat.Client
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Text.Unicode;
+
+namespace Chat.Client
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const int _serverPort = 9901;
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var udpClient = new UdpClient(/*9910*/);
+            await udpClient.SendAsync(Encoding.UTF8.GetBytes("Hello I'm a new client"), new IPEndPoint(IPAddress.Broadcast, 9910));
+
+            Task.Run(async () =>
+            {
+                do
+                {
+                    var result = await udpClient.ReceiveAsync();
+                    Console.WriteLine($"{Encoding.UTF8.GetString(result.Buffer)} -> {result.RemoteEndPoint}");
+                }
+                while (true);
+            });
+
+            using var tcpClient = new TcpClient();
+            tcpClient.Connect(System.Net.IPAddress.Loopback, _serverPort);
+
+            var stream = tcpClient.GetStream();
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+
+            var readerTask = Task.Run(() =>
+            {
+                string? message = null;
+                do
+                {
+                    message = reader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                while (true);
+            });
+
+            var writerTask = Task.Run(() =>
+            {
+                do
+                {
+                    var message = Console.ReadLine();
+                    writer.WriteLine(message);
+                    writer.Flush();
+                }
+                while (true);
+            });
+
+            await Task.WhenAll(readerTask, writerTask);
         }
     }
 }
--- Chat.Server/Program.cs
+++ Chat.Server/Program.cs
@@ -1,10 +1,122 @@
-﻿namespace Chat.Server
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat.Server
 {
+    class ChatClient:IDisposable
+    { 
+        private readonly TcpClient _tcpClient;
+        private readonly NetworkStream _stream;
+        private readonly StreamReader _reader;
+        private readonly StreamWriter _writer;
+
+        public TcpClient Client => _tcpClient;
+        public ChatClient(TcpClient tcpClient)
+        { 
+            _tcpClient = tcpClient;
+
+            _stream = _tcpClient.GetStream();
+            _reader = new StreamReader(_stream);
+            _writer = new StreamWriter(_stream);
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        public Task Run()
+        {
+            return Task.Run( () => 
+            { 
+                string? message = null;
+                do
+                {
+                    message = _reader.ReadLine();
+                    Log(message);
+                    MessageReceived?.Invoke(this, new ChatMessageEventArgs(message));
+                }
+                while (!string.IsNullOrEmpty(message));
+            });
+        }
+
+        public async Task SendMessage(string? message)
+        { 
+            await _writer.WriteLineAsync(message);
+            await _writer.FlushAsync();
+        }
+
+        private void Log(string? message)
+        { 
+            Console.WriteLine($"[{_tcpClient.Client.RemoteEndPoint}]: {message}");
+        }
+
+        public event EventHandler<ChatMessageEventArgs>? MessageReceived;
+    }
+
+    class ChatMessageEventArgs : EventArgs
+    {
+        public string? Message { get; }
+
+        public ChatMessageEventArgs(string? message)
+        {
+            Message = message;
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        private const int _port = 9901;
+        private static List<ChatClient> _clients = new List<ChatClient>();
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var udpClient = new UdpClient(9910);
+            Task.Run(async () =>
+            {
+                do
+                {
+                    var result = await udpClient.ReceiveAsync();
+                    Console.WriteLine($"{Encoding.UTF8.GetString(result.Buffer)} -> {result.RemoteEndPoint}");
+                }
+                while (true);
+            });
+
+            var server = new TcpListener(System.Net.IPAddress.Any, _port);
+            server.Start();
+
+            try
+            {
+                while (true)
+                {
+                    var tcpClient = await server.AcceptTcpClientAsync();
+                    var client = new ChatClient(tcpClient);
+                    client.MessageReceived += Client_MessageReceived;
+
+                    _clients.Add(client);
+                    var task = client.Run();
+                }
+            }
+            finally
+            { 
+                server.Stop();
+            }
+        }
+
+        private static void Client_MessageReceived(object? sender, ChatMessageEventArgs e)
+        {
+            var client = sender as ChatClient;
+            if (client == null)
+                return;
+
+            var message = $"[{client.Client.Client.RemoteEndPoint}]: {e.Message}";
+
+            //var sended = new List<Task>(_clients.Count);
+            foreach (var item in _clients)
+            {
+                //sended.Add(item.SendMessage(message));
+                var t = item.SendMessage(message);
+            }
         }
     }
 }
